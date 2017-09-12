d80e149cda73524d1b2adb019002933c5c175c39
cfc603 <wtrevor162@gmail.com>
2017-09-12 11:42:07 -0400

    fix typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
     </nav>
 
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('@yield('header_image')')">
+    <header class="masthead" style="background-image: url(@yield('header_image'))">
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
